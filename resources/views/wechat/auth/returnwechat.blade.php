910542042c9b408ff4fe66503b9f968f8b7719cc
jicb <1050315624@qq.com>
2016-09-19 23:14:57 +0800

    xxx

--- resources/views/wechat/auth/returnwechat.blade.php
+++ resources/views/wechat/auth/returnwechat.blade.php
@@ -3,19 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>返回微信</title>
-    <script src="/js/jweixin.js"></script>
+    {{--<script src="/js/jweixin.js"></script>--}}
     <script>
         window.onload = function(){
-
-            /*WeixinJSBridge.invoke('closeWindow',{},function(res){
+            WeixinJSBridge.invoke('closeWindow',{},function(res){
 
                 //alert(res.err_msg);
-                /!*$.get('/h5/auth/returnregistered?openid={!! $openid !!}',function(res){
+                //
+                /*$.get('/h5/auth/returnregistered?openid={!! $openid !!}',function(res){
 
-                });*!/
+                });*/
 
-            });*/
-            wx.closeWindow();
+            });
+            //wx.closeWindow();
         }
     </script>
 </head>
