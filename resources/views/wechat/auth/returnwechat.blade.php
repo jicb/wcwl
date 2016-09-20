e4451fc3c0afe01e095034911fef8959f0176269
jicb <1050315624@qq.com>
2016-09-20 10:51:28 +0800

    xxd

--- resources/views/wechat/auth/returnwechat.blade.php
+++ resources/views/wechat/auth/returnwechat.blade.php
@@ -3,20 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <title>返回微信</title>
+    {{--<script src="/js/jweixin.js"></script>--}}
     <script>
-        var readyFunc = function onBridgeReady() {
-            WeixinJSBridge.invoke('closeWindow',{
-            },function(res){
+        /*window.onload = function(){
+         WeixinJSBridge.invoke('closeWindow',{},function(res){
 
-            });
+         //alert(res.err_msg);
+         //
+         /!*$.get('/h5/auth/returnregistered?openid={!! $openid !!}',function(res){
 
+         });*!/
 
-        }
+         });
+         //wx.closeWindow();
 
+         }*/
         window.onload = readyFunc;
     </script>
 </head>
 <body>
-<p style="display: none;">welcome</p>
+<button type="type" id="closeWindow">关闭网页</button>
+<script>
+    var readyFunc = function onBridgeReady() {
+        var curid;
+        var curAudioId;
+        var playStatus = 0;
+
+
+        // 关闭当前webview窗口 - closeWindow
+        //document.querySelector('#closeWindow').addEventListener('click', function(e){
+        WeixinJSBridge.invoke('closeWindow',{
+        },function(res){
+
+            //alert(res.err_msg);
+
+        });
+        //});
+
+    }
+
+
+    if (typeof WeixinJSBridge === "undefined") {
+        document.addEventListener('WeixinJSBridgeReady', readyFunc, false);
+    } else {
+        readyFunc();
+    }
+</script>
 </body>
 </html>
