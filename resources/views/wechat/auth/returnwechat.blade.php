a93f893323a993a240ae7d9a9e6243132d6de5a6
jicb <1050315624@qq.com>
2016-09-20 10:28:47 +0800

    gg

--- resources/views/wechat/auth/returnwechat.blade.php
+++ resources/views/wechat/auth/returnwechat.blade.php
@@ -5,49 +5,16 @@
     <title>返回微信</title>
     {{--<script src="/js/jweixin.js"></script>--}}
     <script>
-        /*window.onload = function(){
-            WeixinJSBridge.invoke('closeWindow',{},function(res){
-
-                //alert(res.err_msg);
-                //
-                /!*$.get('/h5/auth/returnregistered?openid={!! $openid !!}',function(res){
-
-                });*!/
-
-            });
-            //wx.closeWindow();
-
-        }*/
-        window.onload = readyFunc;
-    </script>
-</head>
-<body>
-<button type="type" id="closeWindow">关闭网页</button>
-<script>
-    var readyFunc = function onBridgeReady() {
-        var curid;
-        var curAudioId;
-        var playStatus = 0;
-
-
-        // 关闭当前webview窗口 - closeWindow
-        //document.querySelector('#closeWindow').addEventListener('click', function(e){
+        window.onload = function onBridgeReady() {
             WeixinJSBridge.invoke('closeWindow',{
             },function(res){
 
-                //alert(res.err_msg);
-
             });
-        //});
 
-    }
+        };
+    </script>
+</head>
+<body>
 
-
-    if (typeof WeixinJSBridge === "undefined") {
-        document.addEventListener('WeixinJSBridgeReady', readyFunc, false);
-    } else {
-        readyFunc();
-    }
-</script>
 </body>
 </html>
