e8c01ab6a84434ed33ad6e45a8a1b77cdb05657f
jicb <1050315624@qq.com>
2016-09-20 10:40:24 +0800

    xx

--- resources/views/wechat/auth/returnwechat.blade.php
+++ resources/views/wechat/auth/returnwechat.blade.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>返回微信</title>
+    <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
     {{--<script src="/js/jweixin.js"></script>--}}
-    <script>
-        window.onload = function onBridgeReady() {
-            WeixinJSBridge.invoke('closeWindow',{
+    <script type="text/javascript" defer>
+            /*WeixinJSBridge.invoke('closeWindow',{
             },function(res){
 
-            });
+            });*/
+            wx.closeWindow();
 
-        };
     </script>
 </head>
 <body>
