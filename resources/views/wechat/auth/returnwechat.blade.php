86313ad691f5aa22f6d5719182f69b2a1b3a8b13
jicb <1050315624@qq.com>
2016-09-20 10:58:09 +0800

    xxd

--- resources/views/wechat/auth/returnwechat.blade.php
+++ resources/views/wechat/auth/returnwechat.blade.php
@@ -5,25 +5,17 @@
     <title>返回微信</title>
     {{--<script src="/js/jweixin.js"></script>--}}
     <script>
-        var readyFunc = function onBridgeReady() {
-            var curid;
-            var curAudioId;
-            var playStatus = 0;
-
-
-
-            WeixinJSBridge.invoke('closeWindow',{
-            },function(res){
-
-            });
-        }
-
-        window.onload = readyFunc;
-
         if (typeof WeixinJSBridge === "undefined") {
             document.addEventListener('WeixinJSBridgeReady', readyFunc, false);
         } else {
             readyFunc();
+        }
+
+        var readyFunc = function onBridgeReady() {
+            WeixinJSBridge.invoke('closeWindow',{
+            },function(res){
+
+            });
         }
     </script>
 </head>
