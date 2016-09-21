8e7e13f5fdd4262fdd9b881924e2b0bbd53488be
jicb <1050315624@qq.com>
2016-09-21 18:02:04 +0800

    ddd

--- resources/views/wechat/logistics/delivery.blade.php
+++ resources/views/wechat/logistics/delivery.blade.php
@@ -45,6 +45,14 @@
             border-right: 1px solid;
         }
 
+        .myfoot{
+            position:fixed;
+            z-index: 999;
+            bottom: 0;
+            left:0;
+            width:100%;
+        }
+
 
 
     </style>
@@ -74,6 +82,56 @@
     </div>
     <hr />
 
+    <div class="row">
+        <div class="pull-left"><span class="glyphicon glyphicon-glass"></span> </div>
+        <div class="pull-left">请输入货物信息</div>
+        <div class="pull-right"><span class="glyphicon glyphicon-chevron-right"></span> </div>
+    </div>
+    <hr />
+
+    <div class="row">
+        <div class="col-xs-6">
+            付款方式
+            <div class="pull-right">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </div>
+        </div>
+        <div class="col-xs-6">
+            付款方式
+            <div class="pull-right">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-6">
+            付款方式
+            <div class="pull-right">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </div>
+        </div>
+        <div class="col-xs-6">
+            付款方式
+            <div class="pull-right">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </div>
+        </div>
+    </div>
+    <hr />
+    <div class="row text-center">
+        备注
+    </div>
+    <div class="myfoot">
+        <div class="row">
+            <div class="col-xs-8 bg-info">
+                同意
+            </div>
+            <div class="col-xs-4 bg-warning">
+                提交
+            </div>
+        </div>
+    </div>
 </div>
+
 </body>
 </html>
