c93f760ab68397ab7351886e457b708c6170e85a
jicb <1050315624@qq.com>
2016-09-21 15:41:33 +0800

    fdf

--- resources/views/wechat/logistics/delivery.blade.php
+++ resources/views/wechat/logistics/delivery.blade.php
@@ -2,9 +2,58 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1，user-scalable=0">
+    <title>我要发货</title>
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="/js/app.js"></script>
+
+    <style>
+        .height1{
+            height:100px;
+        }
+        .shuiping{
+            margin-left:-20%;
+            margin-right:-20%;
+            padding:0;
+            width:200%;
+
+        }
+        p{
+            display:table-cell;
+            vertical-align:middle;
+        }
+
+        .xxx{
+            border-left:1px solid red;
+            height:100px;
+        }
+
+
+    </style>
 </head>
 <body>
-<h1>hello i am delivery!openid={!! $openid !!}</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-9 height1 " style="display:table;">
+            <p>请输入发货信息请输入发货信息请输入发货信息请输入发货信息请输入息请输入发货信息</p>
+        </div>
+        <div class="col-xs-3 height1">
+                添加
+        </div>
+    </div>
+    <hr class="shuiping"/>
+    <div class="row">
+        <div class="col-xs-9 height1 ">
+            请输入收货信息
+        </div>
+        <div class="col-xs-3 height1 ">
+            添加
+        </div>
+    </div>
+</div>
 </body>
 </html>
