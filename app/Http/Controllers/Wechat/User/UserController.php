707adc5d404d2ba79b652da5007a4b5f32dc2f6c
jicb <1050315624@qq.com>
2016-09-21 14:19:29 +0800

    xx

--- app/Http/Controllers/Wechat/User/UserController.php
+++ app/Http/Controllers/Wechat/User/UserController.php
@@ -13,8 +13,8 @@ class UserController extends Controller
     public function adduser(Request $request){
         $wechat = app('wechat');
         $group = $wechat->user_group;
-        $group->moveUser(["oLsBZxNMEZQEL8STHlrEaSu5mwD8"],101);
-        dd($group->userGroup("oLsBZxNMEZQEL8STHlrEaSu5mwD8"));
+        $group->moveUser(["oLsBZxF7dknqqY9oGJtEVreG5Ot4"],101);
+        dd($group->userGroup("oLsBZxF7dknqqY9oGJtEVreG5Ot4"));
     }
 
     public function create(Request $request){
