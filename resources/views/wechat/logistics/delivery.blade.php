7a3c225602dda44499b75e1a54fa5c66360a29a1
jicb <1050315624@qq.com>
2016-09-21 17:23:10 +0800

    ddd

--- resources/views/wechat/logistics/delivery.blade.php
+++ resources/views/wechat/logistics/delivery.blade.php
@@ -54,23 +54,23 @@
 <body>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-xs-10 height1">
+        <div class="col-xs-9 height1">
             <div class="vertical-middle border-right">
                 <p class="text-success bottom0">计长兵 15527219896</p>
                 <p class="text-info bottom0">湖北省 武汉市 江夏区</p>
                 <p class="text-info bottom0">万达小区 2号楼1单元4C</p>
             </div>
         </div>
-        <div class="col-xs-2 height1">
+        <div class="col-xs-3 height1">
             <div class="vertical-middle hr-middle"><button class="btn btn-warning btn-center vertical-middle"><span class="glyphicon glyphicon-pencil"></span> 更新</button></div>
         </div>
     </div>
     <hr />
     <div class="row">
-        <div class="col-xs-10 height1 ">
+        <div class="col-xs-9 height1 ">
             <div class="vertical-middle border-right">请输入收货方信息</div>
         </div>
-        <div class="col-xs-2 height1 ">
+        <div class="col-xs-3 height1 ">
             <div class="vertical-middle hr-middle"><button class="btn btn-warning btn-center vertical-middle"><span class="glyphicon glyphicon-pencil"></span> 更新</button></div>
         </div>
     </div>
