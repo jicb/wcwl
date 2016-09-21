fbcca2637207aacdbe24d811723e44b8bd659471
jicb <1050315624@qq.com>
2016-09-21 17:21:16 +0800

    ddd

--- resources/views/wechat/logistics/delivery.blade.php
+++ resources/views/wechat/logistics/delivery.blade.php
@@ -31,9 +31,12 @@
             vertical-align:middle;
             font-size:12px;
         }
+        .hr-middle{
+            text-align:center;
+        }
         .bottom0{
             margin-bottom: 0;
-            font-size: 12px;
+            font-size: 14px;
         }
         .btn-center{
             margin:0;
@@ -51,24 +54,24 @@
 <body>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-xs-9 height1">
+        <div class="col-xs-10 height1">
             <div class="vertical-middle border-right">
                 <p class="text-success bottom0">计长兵 15527219896</p>
                 <p class="text-info bottom0">湖北省 武汉市 江夏区</p>
                 <p class="text-info bottom0">万达小区 2号楼1单元4C</p>
             </div>
         </div>
-        <div class="col-xs-3 height1">
-            <div class="vertical-middle"><button class="btn btn-warning btn-center vertical-middle"><span class="glyphicon glyphicon-pencil"></span> 更新</button></div>
+        <div class="col-xs-2 height1">
+            <div class="vertical-middle hr-middle"><button class="btn btn-warning btn-center vertical-middle"><span class="glyphicon glyphicon-pencil"></span> 更新</button></div>
         </div>
     </div>
     <hr />
     <div class="row">
-        <div class="col-xs-9 height1 ">
+        <div class="col-xs-10 height1 ">
             <div class="vertical-middle border-right">请输入收货方信息</div>
         </div>
-        <div class="col-xs-3 height1 ">
-            <div class="vertical-middle"><button class="btn btn-warning btn-center vertical-middle"><span class="glyphicon glyphicon-pencil"></span> 更新</button></div>
+        <div class="col-xs-2 height1 ">
+            <div class="vertical-middle hr-middle"><button class="btn btn-warning btn-center vertical-middle"><span class="glyphicon glyphicon-pencil"></span> 更新</button></div>
         </div>
     </div>
     <hr />
