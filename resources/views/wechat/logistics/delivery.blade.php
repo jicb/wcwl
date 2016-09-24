875eec6109b9997a3d6947380e453765361de9cf
jicb <1050315624@qq.com>
2016-09-24 23:14:39 +0800

    xxx

--- resources/views/wechat/logistics/delivery.blade.php
+++ resources/views/wechat/logistics/delivery.blade.php
@@ -1,130 +1,142 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta name="format-detection" content="telephone=no">
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="apple-mobile-web-app-capable" content="no">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expires" content="0">
     <title>我要发货</title>
-    <script type="text/javascript" src="http://tajs.qq.com/stats?sId=37342703" charset="UTF-8"></script>
-    <link rel="stylesheet" href="{!! URL::asset('frozen/css/frozen.css') !!}">
-    <link rel="stylesheet" href="{!! URL::asset('frozen/demo/demo.css') !!}">
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
-    <script src="{!! URL::asset('frozen/lib/zepto.min.js') !!}"></script>
-    <script src="{!! URL::asset('frozen/js/frozen.js') !!}"></script>
-    <style>
-        body {
-            background-color: #ffffff;
-        }
-
-        textarea::-ms-input-placeholder {
-            text-align: center;
-        }
-
-        textarea::-webkit-input-placeholder {
-            text-align: center;
-        }
-
-    </style>
+    <link rel="stylesheet" href="{!! URL::asset('sui/dist/css/sm.min.css') !!}"/>
+    {{--<link rel="stylesheet" href="dist/css/sm-extend.min.css"/>--}}
 </head>
 <body>
-<div class="ui-form">
-    <form action="">
-        <div>
-            <ul class="ui-list ui-list-link ui-border-tb">
-                <li class="ui-border-t" data-href="www.baidu.com">
-                    <div class="ui-list-thumb">
-                        <span style="background-image:url({!! URL::asset('frozen/img/icon/icon_plus_alt2.png') !!})"></span>
-                    </div>
-                    <div class="ui-list-info">
-                        <h4 class="ui-nowrap">发货人信息</h4>
-                        <p class="ui-nowrap">发货地址</p>
-                    </div>
-                </li>
-            </ul>
-            <ul class="ui-list ui-list-link ui-border-tb">
-                <li class="ui-border-t">
-                    <div class="ui-list-thumb">
-                        <span style="background-image:url({!! URL::asset('frozen/img/icon/icon_plus_alt2.png') !!})"></span>
-                    </div>
-                    <div class="ui-list-info">
-                        <h4 class="ui-nowrap">计长兵 15527219896</h4>
-                        <p class="ui-nowrap">湖北省武汉市江夏区光谷大道博彦科技1306号</p>
-                    </div>
-                </li>
-            </ul>
 
-
-            <ul class="ui-list ui-list-one ui-list-link ui-border-tb">
-                <li class="ui-border-t">
-                    <div class="ui-list-thumb">
-                        <span style="background-image:url({!! URL::asset('frozen/img/icon/supermarket7.png') !!})"></span>
+<div class="content">
+    <div class="content-block-title">发/收货方信息</div>
+    <div class="list-block media-list">
+        <ul>
+            <li>
+                <a class="item-link item-content">
+                    <div class="item-inner">
+                        <div class="item-title-row">
+                            <div class="item-title">发货人信息</div>
+                        </div>
+                        <div class="item-subtitle">发货地址</div>
+                        <div class="item-text">详细地址</div>
                     </div>
-                    <div class="ui-list-info">
-                        <h4 class="ui-nowrap ui-txt-muted">货物信息</h4>
+                </a>
+            </li>
+            <li>
+                <a class="item-link item-content">
+                    <div class="item-inner">
+                        <div class="item-title-row">
+                            <div class="item-title">计长兵 15527219896</div>
+                        </div>
+                        <div class="item-subtitle">湖北 武汉 江夏区</div>
+                        <div class="item-text">光谷大道金融港博彦科技五楼风行网</div>
                     </div>
-                </li>
-            </ul>
-            <div class="ui-row" >
-                <div class="ui-col ui-col-50">
-                    <ul class="ui-list ui-list-text ui-list-link ui-border-tb">
-
-                        <li class="ui-border-t ui-col">
-                            <h4>付款方式 <br/><br/>到付</h4>
-                        </li>
-
-                    </ul>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="list-block media-list">
+        <ul>
+            <li class="item-link item-content">
+                <div class="item-media">
+                    <i class="icon" style="width:1.45em;height:1.45em;background-image:url({!! URL::asset('sui/img/logistics/supermarket7.png') !!})"></i>
                 </div>
-                <div class="ui-col ui-col-50">
-                    <ul class="ui-list ui-list-text  ui-border-tb">
-
-                        <li class="ui-border-t ui-col">
-                            <h4>报价费用 <br/><br/>10 元</h4>
-                        </li>
-
-                    </ul>
+                <div class="item-inner">
+                    <div class="item-title">货物信息</div>
                 </div>
-            </div>
-            <div class="ui-row">
-                <div class="ui-col ui-col-50">
-                    <ul class="ui-list ui-list-text ui-list-link ui-border-tb">
+            </li>
+        </ul>
+    </div>
 
-                        <li class="ui-border-t ui-col">
-                            <h4>提货方式 <br/><br/>自提</h4>
-                        </li>
+    <div class="list-block">
+        <ul>
+            <li>
+                <a href="#" class="item-link item-content">
+                    <div class="item-inner">
+                        <div class="item-title">付款方式<span style="padding-left:2em;">到付</span></div>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="item-link item-content">
+                    <div class="item-inner">
+                        <div class="item-title">报价费用<span style="padding-left:2em;">10元</span></div>
+                    </div>
+                </a>
+            </li>
 
-                    </ul>
-                </div>
-                <div class="ui-col ui-col-50">
-                    <ul class="ui-list ui-list-text ui-list-link ui-border-tb">
+            <li>
+                <a href="#" class="item-link item-content">
+                    <div class="item-inner">
+                        <div class="item-title">提货方式<span style="padding-left:2em;">自提</span></div>
+                    </div>
+                </a>
+            </li>
 
-                        <li class="ui-border-t ui-col">
-                            <h4>回单要求 <br/><br/>1 份</h4>
-                        </li>
+            <li>
+                <a href="#" class="item-link item-content">
+                    <div class="item-inner">
+                        <div class="item-title">回单要求<span style="padding-left:2em;">1份</span></div>
+                    </div>
+                </a>
+            </li>
+        </ul>
 
-                    </ul>
-                </div>
-            </div>
-            <div class="ui-form-item ui-form-item-pure ui-border-b">
-                <textarea placeholder="备注"></textarea>
-            </div>
-
-        </div>
-        <div style="position:fixed;left:0;bottom:0;z-index:999;width:100%;">
-            <div class="ui-row">
-                <div class="ui-col ui-col-75" style="height: 60px;background-color: blue">订单总额：￥100元 仅供参考<br/>我同意万程运单条款</div>
-                <div class="ui-col ui-col-25" style="height: 60px;line-height:60px;background-color: yellow">提交订单</div>
-            </div>
-        </div>
-    </form>
+    </div>
+</div>
+<div class="row no-gutter" style="position: fixed;left:0;bottom:0;z-index: 100;width:100%;">
+    <div class="col-75" style="height:4em;background-color: darkseagreen">50%</div>
+    <div class="col-25" style="height:4em;background-color: yellow;line-height: 4em;text-align: center">提交订单</div>
 </div>
 
-<script>
-    $('.ui-list li,.ui-tiled li').click(function () {
-        if ($(this).data('href')) {
-            location.href = $(this).data('href');
-        }
+
+<script type="text/javascript" src="{!! URL::asset('sui/dist/js/zepto.js') !!}"></script>
+<script type="text/javascript" src="{!! URL::asset('sui/dist/js/sm.js' !!}"></script>
+<script type="text/javascript" src="{!! URL::asset('sui/dist/js/sm-city-picker.js') !!}"></script>
+{{--<script type="text/javascript">
+
+    /**
+     * 因为Zepto和jQuery框架的$冲突，又不想清掉jQuery的重置，所以改用Zepto调用
+     * 其中sm.js有修改
+     * 其中的dataPicker在官方文件中不存在，是自己加的
+     */
+
+    Zepto(function () {
+
+        'use strict';
+
+        var _$ = Zepto;
+        _$(".J-choose-address").cityPicker({
+            value: ['湖北', '武汉', '武昌区']
+        });
+
+        _$(".J-choose-sex").picker({
+            toolbarTemplate: '<header class="bar bar-nav">\
+              <button class="button button-link pull-right close-picker">确定</button>\
+              <h1 class="title">请选择性别</h1>\
+              </header>',
+            cols: [
+                {
+                    textAlign: 'center',
+                    values: ['男', '女']
+                }
+            ]
+        });
+
+        _$(".J-choose-birth").datePicker({
+            value: ["2000", "01", "01"]
+        });
+        Zepto.init();
     });
+
 </script>
+--}}
 </body>
 </html>
