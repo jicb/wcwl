098fc15c2713b43783f17a77c850ce155aee5511
jicb <1050315624@qq.com>
2016-09-30 18:37:20 +0800

    xxx

--- app/Wechat/Waybill.php
+++ app/Wechat/Waybill.php
@@ -12,5 +12,5 @@ class Waybill extends Model
     protected $primaryKey = "waybill_id";
     protected $fillable = ['waybill_code','order_id','from_name','from_phone','from_pca','from_street','to_name',
                             'to_phone','to_pca','to_street','cargo_name','cargo_count','cargo_weight','cargo_volume',
-                            'cargo_insure','exange_type','receipt_type','price','comment'];
+                            'cargo_insure','exchange_type','receipt_type','price','comment'];
 }
